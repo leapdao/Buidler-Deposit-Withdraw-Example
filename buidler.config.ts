2438b34547a09681dd2ddb995eb8591fba0ca329
Kevin Ho <kevinjho1996@gmail.com>
2020-08-27 14:58:09 -0400

    add ovm to buidler config (not working yet)

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,16 +1,34 @@
-import { usePlugin, BuidlerConfig } from '@nomiclabs/buidler/config'
-
+import { usePlugin, BuidlerConfig, task } from '@nomiclabs/buidler/config'
+import path = require('path')
 usePlugin('@nomiclabs/buidler-ethers')
 usePlugin('@nomiclabs/buidler-waffle')
+import '@eth-optimism/ovm-toolchain/build/src/buidler-plugins/buidler-ovm-compiler';
+import '@eth-optimism/ovm-toolchain/build/src/buidler-plugins/buidler-ovm-node';
 
-const config: BuidlerConfig = {
+const config = {
   mocha: {
     timeout: 50000,
   },
   solc: {
-    version: "0.5.0",
     optimizer: { enabled: true, runs: 200 },
   },
+  useOvm: true
 }
+
+task('test')
+	.addFlag('ovm', 'compile and run tests with OVM')
+	.setAction(async (taskArguments, bre: any, runSuper) => {
+		const { ovm } = taskArguments;
+
+		if (ovm) {
+      bre.config.solc.path = path.resolve(__dirname, 'node_modules', '@eth-optimism', 'solc')
+      bre.config.mocha.timeout = 500000
+      // bre.config.useOvm = true
+		} else {
+      bre.config.solc.version = "0.5.0"
+    }
+
+		await runSuper(taskArguments);
+	});
 
 export default config
